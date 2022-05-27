b4c6c86978a8a0b3b2b9191e8ba08b6bb5580fe9
cepuii <cepuii13@gmail.com>
2022-05-27 22:30:16 +0300

    Complete first run
    Just infinity cycle

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -2,6 +2,8 @@ package academy.devonline.tictactoe;
 
 public class Launcher {
     public static void main(String[] args) {
-
+        Game game = new Game(new PrintData(), new ComputerMove(),
+            new UserMove(), new WinnerVerifier(), new DrawVerifier());
+        game.play();
     }
 }
