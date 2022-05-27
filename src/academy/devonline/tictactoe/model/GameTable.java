225c937176de722156ff8c4abb4e405bbfb3841e
cepuii <cepuii13@gmail.com>
2022-05-27 22:48:09 +0300

    Add model classes

--- src/academy/devonline/tictactoe/model/GameTable.java
+++ src/academy/devonline/tictactoe/model/GameTable.java
@@ -1,4 +1,23 @@
 package academy.devonline.tictactoe.model;
 
 public class GameTable {
+  
+  private final char[][] table = {
+      {' ', ' ', ' '},
+      {' ', ' ', ' '},
+      {' ', ' ', ' '}
+  };
+  
+  public boolean isEmpty(final Cell cell){
+    return table[cell.row()][cell.col()] == ' ';
+  }
+  
+  public char getSign(final Cell cell){
+    return table[cell.row()][cell.col()];
+  }
+  
+  public void setSign(final Cell cell, final char sign){
+    table[cell.row()][cell.col()] = sign;
+  }
+  record Cell(int row, int col){}
 }
