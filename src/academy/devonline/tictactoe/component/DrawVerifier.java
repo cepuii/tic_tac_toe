cd22deb80acbf1c85b9d2c5c2e8f1b9530fee09f
cepuii <cepuii13@gmail.com>
2022-05-28 15:38:24 +0300

    Implement DrawVerifier component

--- src/academy/devonline/tictactoe/component/DrawVerifier.java
+++ src/academy/devonline/tictactoe/component/DrawVerifier.java
@@ -1,10 +1,18 @@
 package academy.devonline.tictactoe.component;
 
 import academy.devonline.tictactoe.model.GameTable;
+import academy.devonline.tictactoe.model.GameTable.Cell;
 
 public class DrawVerifier {
   
   public boolean isDraw(GameTable gameTable) {
-    return false;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (gameTable.isEmpty(new Cell(i, j))) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
