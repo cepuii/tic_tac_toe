8409249a464f5f59056e14efea07fe6c11c9f8c8
cepuii <cepuii13@gmail.com>
2022-05-28 15:32:00 +0300

    Implement UserMove component

--- src/academy/devonline/tictactoe/component/PrintData.java
+++ src/academy/devonline/tictactoe/component/PrintData.java
@@ -7,11 +7,11 @@ public class PrintData {
   public void printMappingTable() {
     String MAPPING_TABLE = """
         -------------
-        | 7 | 8 | 9 |
+        | 1 | 2 | 3 |
         -------------
         | 4 | 5 | 6 |
         -------------
-        | 1 | 2 | 3 |
+        | 7 | 8 | 9 |
         -------------""";
     System.out.println(MAPPING_TABLE);
   }
--- src/academy/devonline/tictactoe/component/UserMove.java
+++ src/academy/devonline/tictactoe/component/UserMove.java
@@ -1,10 +1,30 @@
 package academy.devonline.tictactoe.component;
 
 import academy.devonline.tictactoe.model.GameTable;
+import academy.devonline.tictactoe.model.GameTable.Cell;
+import java.util.Scanner;
 
 public class UserMove {
   
   public void make(GameTable gameTable) {
-  
+    int number;
+    while (true) {
+      System.out.println("Please type number between 1 and 9:");
+      String line = new Scanner(System.in).nextLine();
+      if (line.length() != 1 || !Character.isDigit(line.charAt(0))) {
+        continue;
+      }
+      number = Integer.parseInt(line) - 1;
+      if (number < 0 || number > 8) {
+        continue;
+      }
+      Cell cell = new Cell(number / 3, number % 3);
+      if (!gameTable.isEmpty(cell)) {
+        System.out.println("Can't make a move, because the cell is not free! Try again!");
+      } else {
+        gameTable.setSign(cell, 'X');
+        return;
+      }
+    }
   }
 }
