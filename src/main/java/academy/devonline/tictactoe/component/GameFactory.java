161c14377bd8da1e98f197f9235cb9ed6ddecfa1
cepuii <cepuii13@gmail.com>
2022-06-07 17:03:52 +0300

    Refactor GameFactory
    Remove logic from constructor to corresponding class

--- src/main/java/academy/devonline/tictactoe/component/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/component/GameFactory.java
@@ -4,10 +4,11 @@ import static academy.devonline.tictactoe.model.PlayerType.USER;
 import static academy.devonline.tictactoe.model.Sign.O;
 import static academy.devonline.tictactoe.model.Sign.X;
 
+import academy.devonline.tictactoe.component.console.ConsoleDataPrinter;
+import academy.devonline.tictactoe.component.console.UserMove;
 import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypad;
 import academy.devonline.tictactoe.model.Player;
 import academy.devonline.tictactoe.model.PlayerType;
-import java.util.Arrays;
 
 public class GameFactory {
   
@@ -15,39 +16,10 @@ public class GameFactory {
   private final PlayerType player2Type;
   
   public GameFactory(String[] args) {
-    PlayerType[] playerTypes = new PlayerType[2];
-    if (args.length == 0) {
-      playerTypes[0] = USER;
-      playerTypes[1] = PlayerType.COMPUTER;
-    } else if (args.length == 1) {
-      playerTypes[0] = USER;
-      if (checkCommandLineArgument(args[0])) {
-        playerTypes[1] = PlayerType.valueOf(args[0].toUpperCase());
-      } else {
-        System.err.printf("Unsupported command line argument: %s\n", args[0]);
-        playerTypes[1] = PlayerType.COMPUTER;
-      }
-    } else {
-      for (String arg : args) {
-        if (checkCommandLineArgument(arg) && playerTypes[1] == null) {
-          if (playerTypes[0] == null) {
-            playerTypes[0] = PlayerType.valueOf(arg.toUpperCase());
-          } else {
-            playerTypes[1] = PlayerType.valueOf(arg.toUpperCase());
-          }
-        } else {
-          System.err.printf("Unsupported command line argument: %s\n", arg);
-        }
-      }
-    }
-    player1Type = playerTypes[0];
-    player2Type = playerTypes[1];
-  }
-  
-  
-  private boolean checkCommandLineArgument(String arg) {
-    return Arrays.stream(PlayerType.values())
-        .anyMatch(playerType -> playerType.name().equalsIgnoreCase(arg));
+    CommandLineArgumentParser.PlayerTypes playerTypes =
+        new CommandLineArgumentParser(args).parse();
+    player1Type = playerTypes.getPlayer1Type();
+    player2Type = playerTypes.getPlayer2Type();
   }
   
   
@@ -67,7 +39,7 @@ public class GameFactory {
     }
     final boolean canSecondPlayerMakeFirstMove = player1Type != player2Type;
     return new Game(
-        new PrintData(cellNumberConverter),
+        new ConsoleDataPrinter(cellNumberConverter),
         player1,
         player2,
         new WinnerVerifier(),
