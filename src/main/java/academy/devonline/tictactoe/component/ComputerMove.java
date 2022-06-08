f0d85e4217531d68406f7ec81d828c7a7451d929
cepuii <cepuii13@gmail.com>
2022-06-09 00:48:59 +0300

    Fix infinitive cycle in ComputerMove class

--- src/main/java/academy/devonline/tictactoe/component/ComputerMove.java
+++ src/main/java/academy/devonline/tictactoe/component/ComputerMove.java
@@ -8,16 +8,22 @@ import java.util.Random;
 public class ComputerMove implements Move {
   
   @Override
-  public void make(final GameTable gameTable, Sign sign) {
-    while (true) {
-      int number = new Random().nextInt(9);
-      int row = number / 3;
-      int col = number % 3;
-      Cell cell = new Cell(row, col);
-      if (gameTable.isEmpty(cell)) {
-        gameTable.setSign(cell, sign);
-        return;
+  public void make(final GameTable gameTable, final Sign sign) {
+    final Cell[] emptyCells = new Cell[9];
+    int count = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        final Cell cell = new Cell(i, j);
+        if (gameTable.isEmpty(cell)) {
+          emptyCells[count++] = cell;
+        }
       }
+    }
+    if (count > 0) {
+      final Cell randomCell = emptyCells[new Random().nextInt(count)];
+      gameTable.setSign(randomCell, sign);
+    } else {
+      throw new IllegalArgumentException("Game table does not contain any empty cell!");
     }
   }
 }
