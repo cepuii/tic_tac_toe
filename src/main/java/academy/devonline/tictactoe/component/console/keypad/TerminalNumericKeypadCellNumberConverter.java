9d18bc53ff874f181e22ae039c1cec6544760ba6
cepuii <cepuii13@gmail.com>
2022-06-09 00:33:46 +0300

    Fix toCell and toNumber in TerminalNumericKeypadCellNumberConverter

--- src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -18,11 +18,21 @@ public class TerminalNumericKeypadCellNumberConverter implements CellNumberConve
   
   @Override
   public Cell toCell(char number) {
+    checkNumber(number);
     return new Cell(number / 3, number % 3);
+  }
+  
+  private void checkNumber(char number) {
+    if (number < '1' || number > '9') {
+      throw new IllegalArgumentException(
+          String.format("Out of range [1:9], number = %c", number));
+    }
   }
   
   @Override
   public char toNumber(Cell cell) {
-    return (char) ('0' + ((cell.getRow() * 3) + (cell.getCol() + 1)));
+    char number = (char) ('0' + ((cell.getRow() * 3) + (cell.getCol() + 1)));
+    checkNumber(number);
+    return number;
   }
 }
